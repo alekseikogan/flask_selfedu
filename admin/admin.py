de9902074aa695e3f5b3b0f4782a612bc4f8bb89
Алексей Коган (рабочий) <alekseikogan@yandex.ru>
2023-10-18 12:13:16 +0300

    made admin panel

--- admin/admin.py
+++ admin/admin.py
@@ -1,11 +1,31 @@
-from flask import (Blueprint, flash, redirect, render_template, request,
+import sqlite3
+
+from flask import (Blueprint, flash, g, redirect, render_template, request,
                    session, url_for)
 
 menu = [{'url': '.index', 'title': 'Панель'},
+        {'url': '.listcars', 'title': 'Список авто'},
+        {'url': '.listusers', 'title': 'Список users'},
+        {'url': 'index', 'title': 'Сайт'},
         {'url': '.logout', 'title': 'Выйти'}]
+
+db = None  # ссылается на соединение с БД
 
 admin = Blueprint(
     'admin', __name__, template_folder='templates', static_folder='static')
+
+
+@admin.before_request
+def before_request():
+    global db
+    db = g.get('link_db')
+
+
+@admin.teardown_request
+def teardown_request(request):
+    global db
+    db = None
+    return request
 
 
 @admin.route('/')
@@ -54,3 +74,41 @@ def logout():
     logout_admin()
 
     return redirect(url_for('.login'))
+
+
+@admin.route('/listcars')
+def listcars():
+    if not isLogged():
+        return redirect(url_for('.login'))
+
+    list = []
+    if db:
+        try:
+            cur = db.cursor()
+            cur.execute(f'SELECT title, text, url FROM posts')
+            list = cur.fetchall()
+        except sqlite3.Error as e:
+            print("Ошибка получения статей из БД " + str(e))
+    return render_template(
+        'admin/listcars.html',
+        title='Список авто',
+        menu=menu, list=list)
+
+
+@admin.route('/listusers')
+def listusers():
+    if not isLogged():
+        return redirect(url_for('.login'))
+
+    list = []
+    if db:
+        try:
+            cur = db.cursor()
+            cur.execute(f'SELECT name, email FROM users ORDER BY time DESC')
+            list = cur.fetchall()
+        except sqlite3.Error as e:
+            print("Ошибка получения пользователетей из БД " + str(e))
+    return render_template(
+        'admin/listusers.html',
+        title='Список пользователей',
+        menu=menu, list=list)
